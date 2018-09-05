3cd6773d56f8981eee9a1f088b840e8280cda276
Reece Gao <rgao@gatech.edu>
2018-09-04 21:15:30 -0400

    fixed my error and corrected my gtid

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -44,7 +44,7 @@ public class Person3 {
 	private String calc(String input) {
 	  //Person 3 put your implementation here
 	  StringBuilder s1 = new StringBuilder();
-	  for (int i = input.length() - 1; i >= 0; i++) {
+	  for (int i = (input.length() - 1); i >= 0; i--) {
       	s1.append(input.charAt(i));
       }
 	  return s1.toString();
